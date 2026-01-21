1f398584f64ae51cae9744d451200ca50205304c
Caio da Silva <caiosil.viana@gmail.com>
2026-01-20 22:59:26 -0300

    Clear code

--- server/Servidor.java
+++ server/Servidor.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package servidor;
+package server;
 
 import java.io.IOException;
 import java.io.PrintWriter;
